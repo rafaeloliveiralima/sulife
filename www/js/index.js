309ce467028700fb867b4fedfaaf16dbb37d64dc
Rafael Lima <rafaeloliveiralima@ibest.com.br>
2020-07-30 13:02:39 -0300

    update new
    vawvaw

--- www/js/index.js
+++ www/js/index.js
@@ -17,57 +17,8 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-		checkSMSPermission: function() {
-        var success = function (hasPermission) { 
-            if (hasPermission) {
-				var number = '+5521980327479' /* iOS: ensure number is actually a string */
-				var message = 'TESTE DE ENVIO DE SMS';
-				alert("number=" + number + ", message= " + message);
-		 
-				//CONFIGURATION
-				var options = {
-					replaceLineBreaks: false, // true to replace \n by a new line, false by default
-					android: {
-						intent: 'INTENT'  // send SMS with the native android SMS messaging
-						//intent: '' // send SMS without opening any other app
-					}
-				};
-		 
-				var success = function () { alert('Message sent successfully'); };
-				var error = function (e) { alert('Message Failed:' + e); };
-				sms.send(number, message, options, success, error);
-				//sms.send(...);
-            }
-            else {
-				alert('sem permissao');
-                // show a helpful message to explain why you need to require the permission to send a SMS
-                // read http://developer.android.com/training/permissions/requesting.html#explain for more best practices
-            }
-        };
-        var error = function (e) { alert('Something went wrong:' + e); };
-        sms.hasPermission(success, error);
-    },
-    requestSMSPermission: function() {
-        var success = function (hasPermission) { 
-            if (!hasPermission) {
-                sms.requestPermission(function() {
-                    alert('[OK] Permission accepted')
-                }, function(error) {
-                    alert('[WARN] Permission not accepted')
-                    // Handle permission not accepted
-                })
-            }
-        };
-        var error = function (e) { alert('Something went wrong:' + e); };
-        sms.hasPermission(success, error);
-    },
-	
     initialize: function() {
-		alert('1');
         this.bindEvents();
-		this.checkSMSPermission();
-		alert('2');
     },
     // Bind Event Listeners
     //
